0574afca7b5764c4f346a40ec9527acb13328704
AnonymousTechDeveloper <anshmalviya54@gmail.com>
2025-11-07 07:17:04 +0530

    hlsdbvlsdbvrvbsr WAKE UP! dbvsbvjbvbsvfr MAKE UP!

--- src/Components/ScrollContainer/ScrollContainer.tsx
+++ src/Components/ScrollContainer/ScrollContainer.tsx
@@ -1,46 +1,56 @@
-import type { Stucca } from "../../App";
+import type { Phase, Stucca } from "../../App";
 import styles from "./ScrollContainer.module.scss";
 import scrollImg from "../../assets/images/scroll/Scroll.png";
 import something from "../../assets/images/scroll/something.png";
 import scrollTexture from "../../assets/images/scroll/scrolltexture.jpg";
 
 interface ScrollContainerProfs {
-  stucca: Stucca;
-  className?: string;
+    stucca: Stucca;
+    phase: Phase;
+    className?: string;
+}
+
+const phaseClass = {
+    "initial": "",
+    "active": styles.active,
+    "closing": styles.closing,
 }
 
 export default function ScrollContainer({
-  stucca,
-  className = "",
+    stucca,
+    phase,
+    className = "",
+
 }: ScrollContainerProfs) {
-  return (
-    <div className={className}>
-      <div className={styles.scrollContainer}>
-        <div
-          className={styles.scroll}
-          style={{ backgroundImage: `url(${scrollImg})` }}
-        />
-        <div
-          className={styles.scrollPaper}
-          style={{ backgroundImage: `url(${scrollTexture})` }}
-        >
-          <div className={styles.content}>
-            <div className={styles.somethingTop}>
-              <img src={something} alt="decorative element" />
+
+    return (
+        <div className={className}>
+            <div className={`${styles.scrollContainer} ${phaseClass[phase]}`}>
+                <div
+                    className={styles.scroll}
+                    style={{ backgroundImage: `url(${scrollImg})` }}
+                />
+                <div
+                    className={styles.scrollPaper}
+                    style={{ backgroundImage: `url(${scrollTexture})` }}
+                >
+                    <div className={styles.content}>
+                        <div className={styles.somethingTop}>
+                            <img src={something} alt="decorative element" />
+                        </div>
+                        <div className={styles.somethingBottom}>
+                            <img src={something} alt="decorative element" />
+                        </div>
+                        <div className={styles.imageContainer}>
+                            <img src={stucca.imgLink} alt={stucca.name} />
+                        </div>
+                        <div className={styles.textContainer}>
+                            <h2 className={styles.name}>{stucca.name}</h2>
+                            <p className={styles.depName}>{stucca.depName}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={styles.somethingBottom}>
-              <img src={something} alt="decorative element" />
-            </div>
-            <div className={styles.imageContainer}>
-              <img src={stucca.imgLink} alt={stucca.name} />
-            </div>
-            <div className={styles.textContainer}>
-              <h2 className={styles.name}>{stucca.name}</h2>
-              <p className={styles.depName}>{stucca.depName}</p>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
